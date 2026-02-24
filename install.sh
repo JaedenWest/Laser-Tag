f3ef4e94234b4454ae07555b692f36416b06464b
anastassty <123618961+anastassty@users.noreply.github.com>
2026-02-24 16:52:55 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "ERROR: Virtual environment not active."
+  echo "Run: source .venv/bin/activate"
+  exit 1
+fi
+
 #Install system dependencies 
 sudo apt update
 sudo apt install -y python3 python3-venv python3-tk python3-pip libpq-dev build-essential python3-dev libjpeg-dev zlib1g-dev
